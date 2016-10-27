b8a0e22ede750ac70193798a8d7f883ffad9ab5d
marcusmellis89 <m.m.ellis89@gmail.com>
2016-10-27 09:29:44 -0700

    initial commit

---
+++ excercises/ex2.py
@@ -0,0 +1,11 @@
+one = "ghostface"
+two = "gza"
+three = "rza"
+four = "raekwon"
+five = "cappadonna"
+six = "ODB"
+seven = "method man"
+
+print "the leader of the wu is ", three," because he is the head"
+print "the main lyricist is ", two, " which is short for genius"
+print "the star is ", seven, "because of the charisma"
