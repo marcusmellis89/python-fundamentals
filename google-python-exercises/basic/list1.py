be226fab880a514fa49e51b644feab3c4c5ec946
marcusmellis89 <m.m.ellis89@gmail.com>
2016-10-30 23:47:11 -0700

    [list] problem A

--- google-python-exercises/basic/list1.py
+++ google-python-exercises/basic/list1.py
@@ -21,8 +21,13 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
+  count = 0
   # +++your code here+++
-  return
+  for word in words:
+      if(len(word) >= 2) and (word[len(word) - 1] == word[0]):
+          count += 1
+
+  return count
 
 
 # B. front_x
@@ -75,7 +80,7 @@ def main():
   test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
        ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
-       
+
   print
   print 'sort_last'
   test(sort_last([(1, 3), (3, 2), (2, 1)]),
